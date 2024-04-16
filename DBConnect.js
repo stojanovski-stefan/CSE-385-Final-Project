2a26e7207c5b2204b1956c8066ea685bce5bfdef
Stefan <105825004+sstoj1@users.noreply.github.com>
2024-04-16 00:40:09 -0400

    create server to database api

--- DBConnect.js
+++ DBConnect.js
@@ -1,38 +1,38 @@
 var mysql = require("mysql");
+const PropertiesReader = require("properties-reader");
+const express = require("express");
+const path = require("path");
+
+const properties = PropertiesReader("./db.properties");
+const server = express();
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "EtxiZU5{LmRdFyPGA&(",
-  port: 3306,
+  host: properties.get("DB_HOST"),
+  user: properties.get("DB_USER"),
+  password: properties.get("DB_PASSWORD"),
+  port: properties.get("DB_PORT"),
   // required for older versions of mySQL community server (8.0.36)
-  authPlugin: "mysql_native_password",
+  authPlugin: properties.get("DB_AUTH_PLUGIN"),
 });
 
-connection.connect(function (err) {
-  if (err) {
-    throw err;
-  }
-  console.log("Connected!");
+server.get("/api/v1/data", (req, res) => {
+  let searchTerm = req.query.searchTerm;
+  let sql = `SELECT name, id, role FROM netflix.credits WHERE name = '${searchTerm}' LIMIT 10;`;
 
-  // DB query
-  let sql =
-    "SELECT name, id, `character`, role FROM netflix.credits WHERE name = 'Vladimir Kulich' LIMIT 1;";
-  connection.query(sql, function (err, result) {
-    if (err) {
-      throw err;
-    }
-    // console.log(JSON.stringify(result));
-    console.log(result);
-  });
-
-  // stop DB connection
-  connection.end((error) => {
+  connection.query(sql, (error, results, fields) => {
     if (error) {
-      console.error("Error closing MySQL connection:", error);
-      return;
+      console.error("Error fetching data:", error);
+      res.status(500).json({ error: "An error occurred" });
+    } else {
+      res.json(results); // Return data as JSON
     }
-
-    console.log("MySQL connection closed.");
   });
+});
+
+server.use(express.static(path.join(__dirname, "html")));
+
+// Start the server, listens on port 3000
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
