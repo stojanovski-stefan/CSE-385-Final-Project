bcd7bdfbf0f557bc16a6f97697f668aaabb341be
Stefan <105825004+sstoj1@users.noreply.github.com>
2024-05-09 10:51:44 -0400

    Fix crashing bug when filtering for fiction books

--- html/js/queries.js
+++ html/js/queries.js
@@ -1,6 +1,6 @@
 const queries = {
   allBooks: `SELECT  *  FROM bookdata.books_data ORDER BY ratingsCount DESC LIMIT 30;`,
-  fictionBooks: `SELECT * FROM bookdata.books_data WHERE categories LIKE '%fiction%' && categories NOT LIKE '%nonfiction%';`,
+  fictionBooks: `SELECT * FROM bookdata.books_data WHERE categories LIKE '%fiction%' && categories NOT LIKE '%nonfiction%' LIMIT 100;`,
   nonfictionBooks: `SELECT * FROM bookdata.books_data WHERE categories LIKE '%nonfiction%';`,
   biographyBooks: `SELECT * FROM bookdata.books_data WHERE categories LIKE '%biography%' && categories NOT LIKE '%autobiography%';`,
   mysteryBooks: `SELECT * FROM bookdata.books_data WHERE categories LIKE '%mystery%';`,
